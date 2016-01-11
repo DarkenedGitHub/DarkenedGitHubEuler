ee24553808a48356f4c41ec844740efd68df2cbe
felixk <Felix.Krueger@bredex.de>
2016-01-11 17:22:36 +0100

    Problem #37

---
+++ ProjectEuler/src/de/darkened/projecteuler/problems/P0037.java
@@ -0,0 +1,43 @@
+package de.darkened.projecteuler.problems;
+
+import de.darkened.projecteuler.util.PrimeTable;
+import de.darkened.projecteuler.util.Timer;
+
+public class P0037 {
+    
+    public static void main(String[] args) {
+        Timer.start();
+        
+        PrimeTable table = new PrimeTable();
+        
+        int sum = 0;
+        int count = 0;
+        for (int primeIndex = 0; count < 11; primeIndex++) {
+            int prime = table.getPrime(primeIndex);
+            if (prime < 10) {
+                continue;
+            }
+            boolean truncatable = true;
+            for (int truncatedRight = prime / 10; truncatedRight > 0 && truncatable; truncatedRight /= 10) {
+                if (!table.isPrime(truncatedRight)) {
+                    truncatable = false;
+                }
+            }
+            int pow10 = 10;
+            for (int truncatedLeft = prime % pow10; truncatedLeft < prime && truncatable; truncatedLeft = prime % pow10) {
+                if (!table.isPrime(truncatedLeft)) {
+                    truncatable = false;
+                }
+                pow10 *= 10;
+            }
+            if (truncatable) {
+                sum += prime;
+                count++;
+            }
+        }
+        System.out.println(sum);
+        
+        Timer.stop();
+    }
+
+}
