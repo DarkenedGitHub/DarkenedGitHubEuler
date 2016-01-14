ffb64ecc2994c429c02975799e5db00bbbec8659
felixk <Felix.Krueger@bredex.de>
2016-01-14 09:40:50 +0100

    Problem #45

---
+++ ProjectEuler/src/de/darkened/projecteuler/problems/P0045.java
@@ -0,0 +1,31 @@
+package de.darkened.projecteuler.problems;
+
+import de.darkened.projecteuler.util.Timer;
+
+public class P0045 {
+    
+    public static void main(String[] args) {
+        Timer.start();
+        
+        // T(n)=H(m) for n=2m-1 --> only check pentagonal and hexagonal
+        long p = 40756;
+        long pIndex = 165;
+        long h = 40755;
+        long hIndex = 143;
+
+        while (p != h) {
+            while (p < h) {
+                pIndex++;
+                p = (pIndex) * (3 * pIndex - 1) / 2;
+            }
+            while (h < p) {
+                hIndex++;
+                h = (hIndex) * (2 * hIndex - 1);
+            }
+        }
+        System.out.println(p);
+        
+        Timer.stop();
+    }
+
+}
